283964c3ae66aed950842a2902fb79f603ed4c4d
zowork <sdkwork@zowalk.com>
2026-08-09 20:31:29 +0800

    chore: update 2 files
    Changed: Cargo.toml, crates/sdkwork-routes-customerservice-internal-api/src/http_route_manifest.rs

--- crates/sdkwork-routes-customerservice-internal-api/src/http_route_manifest.rs
+++ crates/sdkwork-routes-customerservice-internal-api/src/http_route_manifest.rs
@@ -1,31 +1,31 @@
 use sdkwork_web_core::{HttpMethod, HttpRoute, HttpRouteManifest};
 
 const HTTP_ROUTES: &[HttpRoute] = &[
-    HttpRoute::api_key(
+    HttpRoute::ingress_token(
         HttpMethod::Post,
         "/internal/v3/api/customer_services/plugins/{pluginCode}/accounts/{accountId}/start",
         "customerservice",
         "customerservice.plugins.internal.accounts.start",
     ),
-    HttpRoute::api_key(
+    HttpRoute::ingress_token(
         HttpMethod::Post,
         "/internal/v3/api/customer_services/plugins/{pluginCode}/accounts/{accountId}/stop",
         "customerservice",
         "customerservice.plugins.internal.accounts.stop",
     ),
-    HttpRoute::api_key(
+    HttpRoute::ingress_token(
         HttpMethod::Get,
         "/internal/v3/api/customer_services/plugins/{pluginCode}/accounts/{accountId}/status",
         "customerservice",
         "customerservice.plugins.internal.accounts.status",
     ),
-    HttpRoute::api_key(
+    HttpRoute::ingress_token(
         HttpMethod::Post,
         "/internal/v3/api/customer_services/plugins/{pluginCode}/accounts/{accountId}/send_message",
         "customerservice",
         "customerservice.plugins.internal.accounts.sendMessage",
     ),
-    HttpRoute::api_key(
+    HttpRoute::ingress_token(
         HttpMethod::Post,
         "/internal/v3/api/customer_services/plugins/{pluginCode}/accounts/{accountId}/delivery_pre_check",
         "customerservice",
